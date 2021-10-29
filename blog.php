7e11258533f83961df5569275aa914a149ec80c0
sdrartus <stephartus20@gmail.com>
2021-10-29 21:37:33 +0800

    Add files via upload

---
+++ blog.php
@@ -0,0 +1,306 @@
+<?php
+$title = "Blog";
+?>
+<html>
+
+<head>
+    <title>Stephanie Artus</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous">
+    </script>
+
+
+</head>
+<style>
+#itzy {
+    background-image:
+        url("https://i.pinimg.com/originals/50/11/8a/50118aaf1a302c128104215f6472fe74.jpg");
+    height: 400px;
+}
+
+@media (min-width: 992px) {
+    #itzy {
+        margin-top: -58.59px;
+    }
+}
+</style>
+<script>
+function showBody() {
+    alert("Hello!");
+}
+</script>
+
+
+<div id="itzy" class="bg-image shadow-2-strong">
+    <div class="mask" style="background-color: rgba(0, 0, 0, 0.8);">
+        <div class="container d-flex align-items-center justify-content-center text-center h-100">
+            <div class="text-white">
+                <h1 class="mb-3">Rising Kpop Girl Group, ITZY!</h1>
+                <h5 class="mb-4">Be one of them, register now!</h5>
+                <button class="btn btn-outline-light btn-lg m-2" data-toggle="modal"
+                    data-target="#modalRegisterForm">Sign
+                    Up</button>
+                <a class="btn btn-outline-light btn-lg m-2" data-toggle="modal" data-target="#modalLoginForm" href=""
+                    role="button">Login</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+<body>
+    <div class="container ">
+
+        <div class="row m-5 p-5">
+
+            <div class="card m-1" style="width: 15rem;">
+                <img class="card-img-top" src="https://i.pinimg.com/564x/0b/e9/d7/0be9d780fb95893ac9d26b53ff06a2b1.jpg"
+                    alt="Hwang Yeji">
+                <div class="card-body">
+                    <h5 class="card-title">Hwang Yeji</h5>
+                    <p class="card-text">ITZY's leader, Hwang Yeji never fail to steal MIDZYs heart with her cat
+                        eyes, love
+                        for snacks, her vocals, </p>
+                    <br><br>
+
+                </div>
+            </div>
+            <div class="card m-1" style="width: 15rem;">
+                <img class="card-img-top" src="https://i.pinimg.com/564x/0e/66/a3/0e66a3d2a10a4d236e0fc9331c5c4286.jpg"
+                    alt="Choi Lia">
+                <div class="card-body">
+                    <h5 class="card-title">Choi Jisu</h5>
+                    <p class="card-text">The softest, purest, cutest lead vocalist. She can eat anything aside
+                        from
+                        cucumber.</p>
+                    <br><br>
+                </div>
+            </div>
+            <div class="card m-1" style="width: 15rem;">
+                <img class="card-img-top" src="https://i.pinimg.com/564x/13/35/31/13353176bb3ed07b1e15fa0af2adc96c.jpg"
+                    alt="Shin Ryujin">
+                <div class="card-body">
+                    <h5 class="card-title">Shin Ryujin</h5>
+                    <p class="card-text">With her deep husky voice, whisker's dimple, and amazing all rounder
+                        skills, indeed
+                        she is ITZY's center.</p>
+                    <br><br>
+                </div>
+            </div>
+            <div class="card m-1" style="width: 15rem;">
+                <img class="card-img-top" src="https://i.pinimg.com/564x/09/ff/2b/09ff2b79797d6c4cef32251ed3fa218a.jpg"
+                    alt="Lee Chaeryeong">
+                <div class="card-body">
+                    <h5 class="card-title">Lee Chaeryeong</h5>
+                    <p class="card-text">Known as Professor Chaeryeong, as she is the prettiest dance professor
+                        for MIDZYs.
+                    </p>
+                    <br><br>
+                </div>
+            </div>
+            <div class="card m-1" style="width: 15rem;">
+                <img class="card-img-top" src="https://i.pinimg.com/564x/19/a6/25/19a6253ed9bb2e987dbcac77df6cf116.jpg"
+                    alt="Shin Yuna">
+                <div class="card-body">
+                    <h5 class="card-title">Shin Yuna</h5>
+                    <p class="card-text">The maknae, yet the tallest of them all. Also known for her remarkable
+                        facial
+                        expression. </p>
+                    <br><br>
+                </div>
+            </div>
+
+
+
+        </div>
+        <main class="mt-5">
+            <div class="container">
+                <!--Section: Content-->
+                <section>
+                    <div class="row">
+                        <div class="col-md-6 gx-5 mb-4">
+                            <div class="bg-image hover-overlay ripple shadow-2-strong rounded-5"
+                                data-mdb-ripple-color="light">
+                                <img src="https://i.pinimg.com/originals/50/11/8a/50118aaf1a302c128104215f6472fe74.jpg"
+                                    class="img-fluid" />
+                                <a href="#!">
+                                    <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+                                </a>
+                            </div>
+                        </div>
+
+                        <div class="col-md-6 gx-5 mb-4">
+                            <h4><strong>Itzy: A Girl Group Blazing a New Path of Its Own</strong></h4>
+                            <h5><strong>Itzy’s members miss their fans, but they have lots in store to reward them for
+                                    listening loyally</strong></h5>
+                            <p class="text-muted">
+                                The group’s five members (who also include Yuna, Ryujin, and Yeji) say they learned how
+                                much they loved to sing and dance by
+                                watching K-pop groups like Girls’ Generation and attending the circus as kids. Prior to
+                                being chosen for Itzy,
+                                several members cut their teeth on Korean reality shows: Chaeryeong appeared on
+                                both K-pop Star 3 and the survival competition Sixteen, while Ryujin and Yeji made
+                                appearances on two other survival series.
+                            </p>
+                            <p><strong>“I really want to take a selfie with the crowd,” Itzy’s Lia says. “I just miss
+                                    them,” Chaeryeong adds.</strong></p>
+                            <p class="text-muted">
+                                That shock doesn’t come across in the music. Itzy’s sound is confident, hyped, and in
+                                your face. And as much as they all look up to the idols that preceded them,
+                                they want to make sure their sound is all their own. “We don’t have a role model,”
+                                declares Yeji. “We just want to be Itzy.”
+                            </p>
+                        </div>
+                    </div>
+                </section>
+                <!--Section: Content-->
+
+                <hr class="my-5" />
+                <main class="mt-5">
+                    <div class="container">
+                        <!--Section: Content-->
+                        <section>
+                            <div class="row">
+                                <div class="col-md-6 gx-5 mb-4">
+                                    <div class="bg-image hover-overlay ripple shadow-2-strong rounded-5"
+                                        data-mdb-ripple-color="light">
+                                        <img src="https://assets.teenvogue.com/photos/5e8dd0005e54a30008c8e382/16:9/w_2560%2Cc_limit/ITZY.jpg"
+                                            class="img-fluid" />
+                                        <a href="#!">
+                                            <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);">
+                                            </div>
+                                        </a>
+                                    </div>
+                                </div>
+
+                                <div class="col-md-6 gx-5 mb-4">
+                                    <h4><strong>Meet ITZY the K-pop girl group taking over the world</strong></h4>
+                                    <h5><strong>Their self-love anthem 'DALLA DALLA' became the quickest video to reach
+                                            100 million views in K-pop history.</strong></h5>
+                                    <p class="text-muted">
+                                        On February 12, 2019, a fresh and powerful wave of talent took over the world.
+                                        The mystically synchronized K-pop girl group ITZY emerged, with a splash of
+                                        their “DALLA DALLA” -- the title for their debut single, meaning different in
+                                        Korean. ITZY’s self-love anthem positioned the band, signed to JYP
+                                        Entertainment, for worldwide success and the premiere of the “DALLA DALLA” music
+                                        video became the quickest to reach 100 million views in K-pop history.
+                                    </p>
+                                    <p><strong>What goals did you have when you debuted, that you wanted to achieve in
+                                            your first year??</strong></p>
+                                    <p class="text-muted">
+                                        Yeji: What we wanted was to make a debut, and we did. After that, we said that
+                                        we wanted to win an award for the best new artist, and we did! We’re so grateful
+                                        that we won. This year’s been very surreal, truly. So many things have happened
+                                        and it’s only been a year!
+                                    </p>
+                                </div>
+                            </div>
+                        </section>
+                        <!--Section: Content-->
+
+                        <hr class="my-5" />
+
+
+
+
+                    </div>
+
+            </div>
+    </div>
+
+
+</body>
+<footer class="bg-light text-lg-start">
+
+
+
+    <!-- Copyright -->
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+        © 2021 Copyright:
+        <a class="text-dark" href="https://www.facebook.com/stephstepstef/">Artus, Stephanie D.R.</a>
+    </div>
+    <!-- Copyright -->
+</footer>
+
+<div class="modal fade" id="modalLoginForm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header text-center">
+                <h4 class="modal-title w-100 font-weight-bold">Log in</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body mx-3">
+                <div class="md-form mb-5">
+                    <i class="fas fa-envelope prefix grey-text"></i>
+                    <input type="email" id="defaultForm-email" class="form-control validate">
+                    <label data-error="wrong" data-success="right" for="defaultForm-email" require>Your email</label>
+                </div>
+
+                <div class="md-form mb-4">
+                    <i class="fas fa-lock prefix grey-text"></i>
+                    <input type="password" id="defaultForm-pass" class="form-control validate">
+                    <label data-error="wrong" data-success="right" for="defaultForm-pass" require>Your password</label>
+                </div>
+
+            </div>
+            <div class="modal-footer d-flex justify-content-center">
+                <a href="main.php">
+                    <button class="btn btn-default">Login</button>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="modalRegisterForm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header text-center">
+                <h4 class="modal-title w-100 font-weight-bold">Sign up</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body mx-3">
+                <div class="md-form mb-5">
+                    <i class="fas fa-user prefix grey-text"></i>
+                    <input type="text" id="orangeForm-name" class="form-control validate">
+                    <label data-error="wrong" data-success="right" for="orangeForm-name" require>Your name</label>
+                </div>
+                <div class="md-form mb-5">
+                    <i class="fas fa-envelope prefix grey-text"></i>
+                    <input type="email" id="orangeForm-email" class="form-control validate">
+                    <label data-error="wrong" data-success="right" for="orangeForm-email" require>Your email</label>
+                </div>
+
+                <div class="md-form mb-4">
+                    <i class="fas fa-lock prefix grey-text"></i>
+                    <input type="password" id="orangeForm-pass" class="form-control validate">
+                    <label data-error="wrong" data-success="right" for="orangeForm-pass" require>Your password</label>
+                </div>
+
+            </div>
+            <div class="modal-footer d-flex justify-content-center">
+                <a href="main.php">
+                    <button class="btn btn-deep-orange">Sign up</button>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+</html>
